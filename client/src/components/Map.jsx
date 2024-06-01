8cfb3eaed5f6c0f03068204371b6b609498542d1
dcduc168 <22520262@gm.uit.edu.vn>
2024-06-01 12:14:13 +0700

    Biggg updateee!!!

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import PropTypes from "prop-types";
 import L from "leaflet";
